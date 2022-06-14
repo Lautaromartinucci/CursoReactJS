8d1b06c853c5edb63623119e6761b9f142a78988
dante tabo <dantetabogmail.com>
2022-06-14 18:23:59 -0300

    Contador

--- my-app/src/components/paginas/ItemCount.js
+++ my-app/src/components/paginas/ItemCount.js
@@ -1,17 +1,35 @@
 import React,{useState} from 'react'
 
-const ItemCount = (ItemCount) => {
-
-    const [contador, setContador] = useState(0);
-
-    return <div>
-        <p>Valor del contador: {contador}</p>
-        <button onClick={() => {setContador( contador - 1)}}>-</button>
-        <button onClick={() => {setContador( contador + 1)}}>+</button>
-        <button>Agregar al carrito</button>
+const ItemCount = () => {
+  const [counter, setCounter, ] = useState(1);
+ 
+  const increase = () => {
+    if (counter < 5) { 
+    setCounter(count => count + 1);
+    }
+  };
+ 
+  const decrease = () => {
+    if (counter > 1) {
+      setCounter(count => count - 1);
+    }
+  };
+ 
+  const reset = () =>{
+    setCounter(1)
+  }
+ 
+  return (
+    <div className="counter">
+      <h1>contador</h1>
+      <span className="counter__output">{counter}</span>
+      <div className="btn__container">
+        <button className="control__btn" onClick={increase}>+</button>
+        <button className="control__btn" onClick={decrease}>-</button>
+        <button className="reset" onClick={reset}>Reset</button>
+      </div>
     </div>
-
-   
+  );
 }
 
 export default ItemCount
